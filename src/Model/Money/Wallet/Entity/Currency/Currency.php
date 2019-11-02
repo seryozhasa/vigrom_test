43bd466d6f24d338a732016b4c3b68c8388d05fa
sergey <seryozhasafonov@gmail.com>
2019-11-02 21:53:23 +0500

    К валюте добавлены аннотации доктрины

--- src/Model/Money/Wallet/Entity/Currency/Currency.php
+++ src/Model/Money/Wallet/Entity/Currency/Currency.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace App\Model\Money\Wallet\Entity\Currency;
 
 use Webmozart\Assert\Assert;
+use Doctrine\ORM\Mapping as ORM;
 
 /**
  * @ORM\Entity
@@ -24,6 +25,7 @@ class Currency
 
     /**
      * @var string
+     * @ORM\Column(type="string")
      */
     private $code;
 
