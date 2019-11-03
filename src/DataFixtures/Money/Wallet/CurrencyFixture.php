b1970a86ffa33c03a4d6a02810ec8f251361f532
sergey <seryozhasafonov@gmail.com>
2019-11-03 12:06:39 +0500

    Добавлена фикстура кошелька

--- src/DataFixtures/Money/Wallet/CurrencyFixture.php
+++ src/DataFixtures/Money/Wallet/CurrencyFixture.php
@@ -14,15 +14,20 @@ use App\Model\Money\Wallet\Entity\Currency\Currency;
  */
 class CurrencyFixture extends Fixture
 {
+    public const REFERENCE_RUB = 'currency_rub';
+    public const REFERENCE_USD = 'currency_usd';
+
     public function load(ObjectManager $manager)
     {
         $rub = Currency::createRUB();
         $rub->setValue(1.0);
         $manager->persist($rub);
+        $this->setReference(self::REFERENCE_RUB, $rub);
 
         $usd = Currency::createUSD();
         $usd->setValue(65.0);
         $manager->persist($usd);
+        $this->setReference(self::REFERENCE_USD, $usd);
 
         $manager->flush();
     }
---
+++ src/DataFixtures/Money/Wallet/WalletFixture.php
@@ -0,0 +1,46 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\DataFixtures\Money\Wallet;
+
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\DataFixtures\DependentFixtureInterface;
+use Doctrine\Common\Persistence\ObjectManager;
+use App\Model\Money\Wallet\Entity\Wallet\Wallet;
+
+/**
+ *
+ * @author sergey seryozhasafonov@gmail.com
+ */
+class WalletFixture extends Fixture implements DependentFixtureInterface
+{
+    public const REFERENCE_RUB = 'wallet_rub';
+    public const REFERENCE_USD = 'wallet_usd';
+
+    public function load(ObjectManager $manager)
+    {
+        $wallet_rub = Wallet::create($this->getReference(CurrencyFixture::REFERENCE_RUB), 1000.0);
+        $manager->persist($wallet_rub);
+        $this->setReference(self::REFERENCE_RUB, $wallet_rub);
+
+        $wallet_usd = Wallet::create($this->getReference(CurrencyFixture::REFERENCE_USD), 1000.0);
+        $manager->persist($wallet_usd);
+        $this->setReference(self::REFERENCE_USD, $wallet_usd);
+
+        $manager->flush();
+    }
+
+    /**
+     * This method must return an array of fixtures classes
+     * on which the implementing class depends on
+     *
+     * @return array
+     */
+    public function getDependencies()
+    {
+        return [
+            CurrencyFixture::class,
+        ];
+    }
+}
